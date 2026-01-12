48a41edf44b80ddb072bfc0512ff1abbc306bca4
kushagraswami5 <102528938+kushagraswami5@users.noreply.github.com>
2026-01-12 21:33:01 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -33,5 +33,11 @@ app.use("/api/auth", authRoutes)
 app.use("/api/admin", adminAuthRoutes)
 app.use("/api/artist", artistOnboardingRoutes)
 
-app.listen(5000, () => console.log("Server running on 5000"))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+
 
