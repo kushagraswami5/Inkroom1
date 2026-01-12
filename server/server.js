9211d69ef3478f6a1665d3e4f2aa641d51658f7b
kushagraswami5 <102528938+kushagraswami5@users.noreply.github.com>
2026-01-12 21:49:46 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -2,25 +2,32 @@ import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+
 import authRoutes from "./routes/authRoutes.js"
 import artistRoutes from "./routes/artistRoutes.js"
 import bookingRoutes from "./routes/bookingRoutes.js"
 import adminAuthRoutes from "./routes/adminAuthRoutes.js"
 import artistOnboardingRoutes from "./routes/artistOnboardingRoutes.js"
 
-
 dotenv.config()
 const app = express()
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  "https://inkroom1.vercel.app"
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://inkroom1.vercel.app"
+    ],
+    credentials: true
+  })
+)
 
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB connected"))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err))
 
 app.get("/", (req, res) => {
   res.send("API running")
@@ -29,7 +36,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes)
 app.use("/api/artists", artistRoutes)
 app.use("/api/bookings", bookingRoutes)
-app.use("/api/auth", authRoutes)
 app.use("/api/admin", adminAuthRoutes)
 app.use("/api/artist", artistOnboardingRoutes)
 
@@ -38,8 +44,3 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-
-
-
-
